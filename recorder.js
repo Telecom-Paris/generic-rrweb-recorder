68d44d5be02393631409c389c7eb53ae6d9f99b6
lubenard <lubenard@student.42.fr>
2020-07-21 14:47:59 +0200

    Recorder (rrweb) does start

--- recorder.js
+++ recorder.js
@@ -1,5 +1,7 @@
 console.log("Auto loading !");
 
+
+
 // This is the default configuration
 let config = {
 	position: "bottom-right",
@@ -8,29 +10,69 @@ let config = {
 
 let isDragged = false;
 let isMenuCreated = false;
+let isMenuOpen = false;
 
-// This function launch the record of the screen
-function launchRecord()
-{
-	if (isDragged == false)
-		console.log("This should have launch the recording");
+let recordButton;
+let replayButton;
+
+let events = [];
+let isActive;
+let interval;
+
+
+function includeScript(file) {
+	var script = document.createElement('script'); 
+	script.src = file;
+	script.type = 'text/javascript'; 
+	document.getElementsByTagName('head').item(0).appendChild(script);
 }
 
-function createMenu()
-{
+// This function launch the record of the screen
+function launchRecord() {
+	console.log("This should have launch the recording");
+	isActive = rrweb.record({
+		emit(event) {
+			// push event into the events array
+			events.push(event);
+		},
+	});
+	interval = setInterval(function () {console.log(events);}, 1000) ;
+}
+
+// This function launch the record of the screen
+function stopRecord() {
+	console.log("This should have launch the recording");
+	if (isActive)
+		isActive();
+	clearInterval(interval);
+
+function createMenu() {
 	console.log("Oh, the menu does not seems to exit, let's create it");
+	//Creating record Button as a child element
+	recordButton = new ButtonHandling(document.body, launchRecord, "?", "");
+	recordButton.createChildButton();
 	isMenuCreated = true;
 }
 
-function openMenu()
-{
-	console.log("Opening the menu");
-	if (!isMenuCreated)
-		createMenu();
+function openMenu() {
+	if (isDragged == false)
+	{
+		console.log("Opening the menu");
+		if (!isMenuCreated)
+			createMenu();
+		if (!isMenuOpen) {
+			recordButton.show();
+			isMenuOpen = true;
+		}
+		else
+		{
+			recordButton.hide();
+			isMenuOpen = false;
+		}
+	}
 }
 
-function buttonPosition(button)
-{
+function buttonPosition(button) {
 	if (config.position.search("bottom") > -1)
 		button.style.bottom = "0";
 	if (config.position.search("top") > -1)
@@ -43,66 +85,73 @@ function buttonPosition(button)
 		button.style.left = "0";
 }
 
-function resetStyle()
-{
+function resetStyle() {
 	document.getElementById("recordingButton").style.left = "0";
 	document.getElementById("recordingButton").style.right = "0";
 }
 
 class ButtonHandling {
-	constructor(context, func, text, icon, isMain) {
+	button;
+
+	constructor(context, func, text, icon) {
 		this.context = context;
 		this.func = func;
 		this.text = text;
 		this.icon = icon;
-		this.isMain = isMain
 	}
-	buttonShow()
-	{
+
+	show() {
 		console.log("My button is showing !");
+		this.button.style.visibility = "visible";
 	}
-	buttonDisapear()
-	{
+
+	hide() {
 		console.log("My button disappear !");
+		this.button.style.visibility = "hidden";
+	}
+
+	createBasicButton() {
+		this.button = document.createElement("input");
+		this.button.type = "button";
+		this.button.value = this.text;
+		this.button.onclick = this.func;
+		this.button.style.borderRadius = "50%";
+		this.button.style.border = "none";
+	}
+
+	createMenuButton() {
+		this.createBasicButton();
+		this.button.id="menuButton";
+		this.button.style.height = "70px";
+		this.button.style.width = "70px";
+		this.button.style.backgroundColor = "#d92027";
+		this.button.style.backgroundImage = "url('media/menu32.png')";
+		this.button.style.backgroundRepeat = "no-repeat";
+		this.button.style.backgroundPosition = "center";
+		if (config.movable) {
+			this.button.style.position = "absolute";
+			this.button.style.cursor = "move";
+		}
+		this.context.appendChild(this.button);
+	}
+
+	createChildButton() {
+		this.createBasicButton();
+		this.button.style.height = "50px";
+		this.button.style.width = "50px";
+		this.button.style.backgroundColor = "yellow";
+		this.context.appendChild(this.button);
 	}
 }
 
-// The goal of this function is to create basic button
-function createButton(context, func, text) {
-	var button = document.createElement("input");
-	button.type = "button";
-	button.value = text;
-	button.id = "recordingButton"
-	button.onclick = func;
-
-	//Determine position from config object
-	//buttonPosition(button);
-
-	button.style.borderRadius = "50%";
-	button.style.height = "100px";
-	button.style.width = "100px";
-	button.style.backgroundColor = "#d92027";
-	button.style.backgroundImage = "url('media/menu32.png')";
-	button.style.backgroundRepeat = "no-repeat"
-	button.style.backgroundPosition = "center"
-
-	if (config.movable) {
-		button.style.position = "absolute";
-		button.style.cursor = "move";
-	}
-	context.appendChild(button);
-}
-
-function finishDragging()
-{
+function finishDragging() {
 	isDragged = false;
 }
 
 // The goal of this function is to make a button movable
-function makeButtonMovable()
-{
+function makeButtonMovable() {
 	//Make the button element draggable:
-	dragElement(document.getElementById("recordingButton"));
+	dragElement(document.getElementById("menuButton"));
 
 	function dragElement(elmnt) {
 		var pos1 = 0, pos2 = 0, mouseX = 0, mouseY = 0;
@@ -127,6 +176,7 @@ function makeButtonMovable()
 		}
 
 		function elementDrag(e) {
+			// If this function is called, then this it not a "click"
 			isClick = false;
 			e = e || window.event;
 			e.preventDefault();
@@ -152,17 +202,17 @@ function makeButtonMovable()
 	}
 }
 
+function createBaseDiv() {
+	
+}
+
 window.onload = function() {
+	includeScript("./rrweb/dist/rrweb.js");
+	createBaseDiv();
 	// We define a button that will launch recording
-	createButton(document.body, openMenu, "");
+	var menuButton = new ButtonHandling(document.body, openMenu, "", "");
+	menuButton.createMenuButton();
+	
 	if (config.movable)
 		makeButtonMovable();
 }
-// Because we are using Materialize, 
-
-/*document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, options);
-  });
-
-*/
