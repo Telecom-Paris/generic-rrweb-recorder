38c1aafec2e0cfddba6002f32a41459806094d98
lubenard <lubenard@student.42.fr>
2020-07-22 17:35:26 +0200

    You can now download your lessons. Still bug to fix

--- recorder.js
+++ recorder.js
@@ -12,63 +12,177 @@ let isMenuOpen = false;
 
 //Displayable menu variable
 let div;
-let menuButton;
 let recordButton;
+let pauseButton;
+let downButton;
 
 //Rrweb variables
+let isRrwebLoaded;
 let events = [];
 let isActive;
 let interval;
+
+// WebAudioRecorder variables
+let isWebAudioRecorderLoaded;
+let recorder;
+let recStream;
+let audioConfig;
+let input, encodingType;
+
+//Download variables
+let isJsZipLoaded;
+let textData; //code of the webpage download.html
+let eventBlob;
+let soundBlob;
 
 let screenSize;
 
 function includeScript(file) {
 	var script = document.createElement('script'); 
 	script.src = file;
-	script.type = 'text/javascript'; 
+	script.type = 'text/javascript';
+	script.onload = function () {
+		return true;
+	}
+	script.onerror = function() {
+		return false;
+	}
 	document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+function configureAudio() {
+	navigator.mediaDevices.getUserMedia({audio: true, video:false}).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
+
+		audioContext = new AudioContext();
+
+		//update the format
+		console.log("Format: 2 channel mp3 @ " + audioContext.sampleRate / 1000 + "kHz");
+
+		//assign to recStream for later use
+		recStream = stream;
+
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		//get the encoding 
+		encodingType = "mp3";
+
+		return {
+			input: input,
+			encoding: encodingType
+		};
+	});
 }
 
 // This function launch the record of the screen
 function launchRecord() {
-	console.log("This should have launch the recording");
+	if (!isDragged) {
+		console.log("This should have launch the recording");
+		// Update the style of record button and the onclick function.
+		document.getElementById('recordButton').style.backgroundColor = "white";
+		document.getElementById('recordButton').style.backgroundImage = "url('media/recording32.png')";
+		document.getElementById('recordButton').style.border = "1px solid black";
+		document.getElementById('recordButton').onclick = stopRecord;
 
-	// Update the style of record button and the onclick function.
-	document.getElementById('recordButton').style.backgroundColor = "white";
-	document.getElementById('recordButton').style.backgroundImage = "url('media/recording32.png')";
-	document.getElementById('recordButton').style.border = "1px solid black";
-	document.getElementById('recordButton').onclick = stopRecord;
+		//Opening the menu to create
+		openMenu();
 
-	isActive = rrweb.record({
-		emit(event) {
-			// push event into the events array
-			events.push(event);
-		},
+		// This function load rrweb from local.
+		// Can also load from URL
+		includeScript("./rrweb/dist/rrweb.js");
+
+		// We include WebAudioRecorder
+		includeScript("./recorder/lib/WebAudioRecorder.js");
+
+		//audioConfig = configureAudio();
+
+		navigator.mediaDevices.getUserMedia({audio: true, video: false}).then(function(stream) {
+		console.log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
+
+		audioContext = new AudioContext();
+
+		//update the format
+		console.log("Format: 2 channel mp3 @ " + audioContext.sampleRate / 1000 + "kHz");
+
+		//assign to recStream for later use
+		recStream = stream;
+
+		/* use the stream */
+		input = audioContext.createMediaStreamSource(stream);
+
+		//get the encoding 
+		encodingType = "mp3";
 	});
-	interval = setInterval(function () {console.log(events);}, 1000) ;
+
+		// We launch sound recording
+		recorder = new WebAudioRecorder(input, {
+			workerDir: "./recorder/lib/",
+			encoding: encodingType,
+			numChannel: 2,
+			onEncoderLoading: function(recorder, encodingType) {
+				console.log("Loading " + encodingType + " encoder...");
+			},
+			onEncoderLoaded: function(recorder, encodingType) {
+				console.log(encodingType + " encoder loaded");
+			}
+		});
+
+		recorder.onComplete = function(recorder, blob) {
+			console.log("Encoding complete");
+			soundBlob = blob;
+			console.log(URL.createObjectURL(blob));
+		}
+
+		recorder.startRecording();
+
+		// We launch Rrweb
+		isActive = rrweb.record({
+			emit(event) {
+				// push event into the events array
+				events.push(event);
+			},
+		});
+		interval = setInterval(function () {console.log(events);}, 1000);
+	}
 }
 
 // This function launch the record of the screen
 function stopRecord() {
 
 	// Restore the style and onclick of recordButton
-	document.getElementById('recordButton').style.backgroundColor = "#fdd03b";
+	document.getElementById('recordButton').style.backgroundColor = "#d92027";
 	document.getElementById('recordButton').style.backgroundImage = "url('media/camera32.png')";
 	document.getElementById('recordButton').style.border = "none";
 	document.getElementById('recordButton').onclick = launchRecord;
 
-	console.log("This should have stop the recording");
+	console.log("The recording has been stopped");
+	// We stop Rrweb
 	if (isActive)
 		isActive();
 	clearInterval(interval);
+
+	// We stop audioRecorder
+	recStream.getAudioTracks()[0].stop();
+	recorder.finishRecording();
+
+	// Set the event as Blob
+	eventBlob = new Blob([JSON.stringify(events)], {type: "application/json"});
+	if (events.length > 2) {
+		div.style.width = "150px";
+		console.log("I can download the page");
+		downButton = new Button(div, downRecord, "downRecord", "", "url('media/down32.png')");
+		downButton.createChildButton();
+		downButton.show();
+	}
 }
 
 // TODO: need to simpilfy this function, maybe remove it
 function createMenu() {
 	console.log("Oh, the menu does not seems to exit, let's create it");
 	//Creating record Button as a child element
-	recordButton = new ButtonHandling(div, launchRecord, "recordButton", "", "url('media/camera32.png')");
-	recordButton.createChildButton();
+	//pauseButton = new Button(div, pauseRecord, "pauseButton", "", "url('media/pause32.png')");
+	//recordButton.createChildButton();
 	isMenuCreated = true;
 }
 
@@ -78,15 +192,76 @@ function openMenu() {
 		if (!isMenuCreated)
 			createMenu();
 		if (!isMenuOpen) {
-			div.style.height = "150px";
-			recordButton.show();
+			div.style.width = "150px";
+			//pauseButton.show();
 			isMenuOpen = true;
 		} else {
-			recordButton.hide();
-			div.style.height = "70px";
+			//pauseButton.hide();
+			div.style.width = "70px";
 			isMenuOpen = false;
 		}
 	}
+}
+
+function saveAs(data, filename)
+{
+	// Create invisible link
+	const a = document.createElement("a");
+	a.style.display = "none";
+	document.body.appendChild(a);
+
+	// Set the HREF to a Blob representation of the data to be downloaded
+	a.href = window.URL.createObjectURL(
+		new Blob([data], { type: "application/zip" })
+	);
+
+  // Use download attribute to set set desired file name
+  a.setAttribute("download", filename);
+
+  // Trigger the download by simulating click
+  a.click();
+
+  // Cleanup
+  window.URL.revokeObjectURL(a.href);
+  document.body.removeChild(a);
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                textData = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+
+// This function launch the download of the record
+function downRecord() {
+	//Load Jszip (minified Because it load faster)
+	includeScript("./jszip/dist/jszip.js", isJsZipLoaded);
+
+	let zip = new JSZip();
+
+	readTextFile("./download.html");
+	// We add thoses files to the zip archive
+	zip.file("download.html", textData);
+	zip.file("data/events.json", eventBlob);
+	zip.file("data/sound.mp3", soundBlob);
+
+	// Once archive has been generated, we can download it
+	zip.generateAsync({type:"blob"})
+	.then(function(content) {
+		saveAs(content, "example.zip");
+	});
 }
 
 function buttonPosition(button) {
@@ -107,7 +282,7 @@ function resetStyle() {
 	document.getElementById("recordingButton").style.right = "0";
 }
 
-class ButtonHandling {
+class Button {
 	button;
 
 	constructor(context, func, id, text, icon) {
@@ -119,12 +294,10 @@ class ButtonHandling {
 	}
 
 	show() {
-		console.log("My button is showing !");
 		this.button.style.visibility = "visible";
 	}
 
 	hide() {
-		console.log("My button disappear !");
 		this.button.style.visibility = "hidden";
 	}
 
@@ -155,13 +328,15 @@ class ButtonHandling {
 
 	createChildButton() {
 		this.createBasicButton();
-		this.button.style.height = "50px";
-		this.button.style.width = "50px";
-		this.button.style.display = "block";
-		this.button.style.margin = "25% auto 15px";
+		this.button.style.height = "70px";
+		this.button.style.width = "70px";
+		//this.button.style.display = "block";
+		//this.button.style.margin = "25% auto 15px";
+		this.button.style.position = "absolute";
 		this.button.style.backgroundColor = "#fdd03b";
 		this.button.style.cursor = "pointer";
-		this.context.prepend(this.button);
+		this.button.style.right = "0px";
+		this.context.appendChild(this.button);
 	}
 }
 
@@ -206,7 +381,6 @@ function makeButtonMovable(button) {
 			pos2 = mouseY - e.clientY;
 			mouseX = e.clientX;
 			mouseY = e.clientY;
-			console.log("screenSize.width = " + screenSize.width + " screenSize.height = " + screenSize.height);
 			// set the element's new position, only if not going out of the window:
 			if (elmnt.offsetLeft - pos1 >= 0 && (elmnt.offsetLeft + 70) - pos1 < screenSize.width
 				&& elmnt.offsetTop - pos2 >= 0 && (elmnt.offsetTop + 70) - pos2 < screenSize.height)
@@ -238,9 +412,6 @@ function createBaseDiv() {
 }
 
 window.onload = function() {
-	// This function load rrweb from local.
-	// Can also load from URL
-	includeScript("./rrweb/dist/rrweb.js");
 
 	// We create a div in which wi will display all menu element as block
 	div = createBaseDiv();
@@ -250,8 +421,8 @@ window.onload = function() {
 	screenSize = window.screen;
 
 	// We define a button that will launch recording
-	menuButton = new ButtonHandling(div, openMenu, "menuButton", "", "url('media/menu32.png')");
-	menuButton.createMenuButton();
+	recordButton = new Button(div, launchRecord, "recordButton", "", "url('media/camera32.png')");
+	recordButton.createMenuButton();
 
 	if (config.movable)
 		makeButtonMovable(div);
